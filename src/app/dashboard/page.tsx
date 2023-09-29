d8f3d97d0bc14d0c0a40b6f6b03f2d029d915c61
Nicholas Pasqualetti <103459977+npasqualetti@users.noreply.github.com>
2023-09-28 21:20:07 -0400

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,39 +1,15 @@
 // @ts-nocheck
 
 "use client";
-import { useState, useEffect } from "react"
-import PeopleDataTable from "@/components/data-table";
-import { columns } from "@/components/columns";
 import { people } from "@/data/people"
-import useUserSubscription from "@/lib/hooks/use-user-subscription"
-import { getSignedInUser } from "@/lib/auth/helper";
 
 export default async function Home() {
-  function getData(name, session) { 
-    if (session?.user?.role == "Guest") {
-      return people
-    } else {
-      if (name == "Free") {
-        return people.slice(0,5)
-      } else if (name == "Basic") {
-        return people.slice(0,1000)
-      } else if (name == "Pro") {
-        return people
-      } else {
-        return []
-      }
-    }
-  }
-  const subscription = await useUserSubscription()
-  const subscriptionName = subscription?.subscriptions[0]?.name
-  const session = await getSignedInUser();
-  const data = getData(subscriptionName, session)
 
   return (
     <>
       <main className='min-h-screen'>
         <div className='p-4 min-h-screen grid md:grid-cols-1 grid-cols-1 gap-4'>
-          <PeopleDataTable columns={columns} data={data} />
+          <PeopleDataTable columns={columns} data={people} />
         </div>
       </main>
     </>
